b10839dfdc7f26a733ee221cf7bf31631b54cd64
Hristo <sandevh@gmail.com>
2024-12-07 14:41:39 -0500

    bug: imports

--- frontend/src/utils/word_semantic.ts
+++ frontend/src/utils/word_semantic.ts
@@ -1,35 +1,35 @@
-import * as tf from "@tensorflow/tfjs";
-import * as use from "@tensorflow-models/universal-sentence-encoder";
-
-export async function compareSentences(sentence1: string, sentence2: string) {
-  const model = await use.load();
-  const embeddings = await model.embed([sentence1, sentence2]);
-
-  const similarity = tf.matMul(
-    embeddings.slice([0, 0], [1]),
-    embeddings.slice([1, 0], [1]),
-    false,
-    true,
-  );
-
-  return similarity.dataSync()[0];
-}
-
-export const getMostSimilar = async (
-  sentence: string,
-  sentences: string[],
-): Promise<{ canonicalTitle: string; similarity: number }> => {
-  const similarities = await Promise.all(
-    sentences.map(
-      async (s): Promise<[number, string]> => [
-        await compareSentences(sentence, s),
-        s,
-      ],
-    ),
-  );
-
-  return {
-    canonicalTitle: similarities.sort((a, b) => a[0] - b[0])[0][1],
-    similarity: similarities.sort((a, b) => a[0] - b[0])[0][0],
-  };
-};
+// import * as tf from "@tensorflow/tfjs";
+// import * as use from "@tensorflow-models/universal-sentence-encoder";
+//
+// export async function compareSentences(sentence1: string, sentence2: string) {
+//   const model = await use.load();
+//   const embeddings = await model.embed([sentence1, sentence2]);
+//
+//   const similarity = tf.matMul(
+//     embeddings.slice([0, 0], [1]),
+//     embeddings.slice([1, 0], [1]),
+//     false,
+//     true,
+//   );
+//
+//   return similarity.dataSync()[0];
+// }
+//
+// export const getMostSimilar = async (
+//   sentence: string,
+//   sentences: string[],
+// ): Promise<{ canonicalTitle: string; similarity: number }> => {
+//   const similarities = await Promise.all(
+//     sentences.map(
+//       async (s): Promise<[number, string]> => [
+//         await compareSentences(sentence, s),
+//         s,
+//       ],
+//     ),
+//   );
+//
+//   return {
+//     canonicalTitle: similarities.sort((a, b) => a[0] - b[0])[0][1],
+//     similarity: similarities.sort((a, b) => a[0] - b[0])[0][0],
+//   };
+// };
